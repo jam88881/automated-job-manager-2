6665459202912b0753e27732328cf0768da77550
John Amarante <john.amarante8888@gmail.com>
2020-03-26 13:10:58 -0700

    correcting syntax errors

--- main.py
+++ main.py
@@ -19,15 +19,19 @@ def run_sql(database, sql):
         logging.info('%s, commited', sql)
     except Exception as ex:
         print(str(ex))
-        logging.error('Error retreiveing data for Factor %s:  %s',  self.factor_name, str(ex))
+        logging.error(str(ex))
 
 def on_created(event):
-    print(f"{event.src_path} has been created")
-    if (".sql" in event.src_path):
-        with open(event.src_path, 'r') as file:
-            contents = f.read()
-            print(contents)
-            run_sql('factors.db', contents)
+    try:
+        time.sleep(1)
+        print(f"{event.src_path} has been created")
+        if (".sql" in event.src_path):
+            with open(event.src_path, 'r') as f:
+                contents = f.read()
+                print(contents)
+                run_sql('factors.db', contents)
+    except Exception as ex:
+        logging.error(str(ex))
 
 def on_deleted(event):
     print(f"deleted {event.src_path}")
@@ -43,19 +47,22 @@ def process_queue():
     for subdir, dirs, files in os.walk('queue'):
         for file in files:
             print(os.path.join(subdir, file))
-            with open(os.path.join(subdir, file), 'r') as file:
+            with open(os.path.join(subdir, file), 'r') as f:
                 contents = f.read()
                 print(contents)
                 run_sql('factors.db',contents)
 
 if __name__ == "__main__":
     #TODO set up a config
-    #check queue directory on start
     try:
-        logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s', level='INFO')
+        logging.basicConfig(
+            format='%(asctime)s [%(levelname)s] %(message)s', 
+            level='INFO',
+            handlers=[logging.FileHandler("app.log"),logging.StreamHandler()])
         logging.info('Initaling AP scheduler to process...')
-
-        s.add_cron_job(process_queue, day_of_week='mon-fri', hour=9, minute=47)
+        
+        #check queue directory on a set schedule
+        s.add_cron_job(process_queue, day_of_week='mon-fri', hour=13, minute=12)
         s.start()
 
         #set up watchdog file system watcher in the root directory
@@ -73,7 +80,6 @@ if __name__ == "__main__":
         observer.schedule(event_handler, path, recursive=True)
         print("starting observer")
         observer.start()
-        os.system('pause')
         try:
             while True:
                 time.sleep(1)
@@ -84,5 +90,3 @@ if __name__ == "__main__":
     except Exception as ex:
         print(str(ex))
         os.system('pause')
-
-    
